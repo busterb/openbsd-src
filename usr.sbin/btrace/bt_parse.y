7f0172fbb79cba21c214f89932fbe9f14a9a51ed
Brent Cook <busterb@gmail.com>
2026-03-20 18:52:48 -0500

    add strncmp support

--- usr.sbin/btrace/bt_parse.y
+++ usr.sbin/btrace/bt_parse.y
@@ -136,7 +136,7 @@ static int 	 beflag = 0;		/* BEGIN/END parsing context flag */
 %token	<v.i>		OP_BANDEQ OP_BOREQ OP_XOREQ OP_SHLEQ OP_SHREQ
 %token	<v.i>		OP_INC OP_DEC
 /* Builtins */
-%token	<v.i>		BUILTIN BEGIN BREAK CONTINUE ELSE END FOR IF KSYM LEN SIZEOF STR USYM WHILE
+%token	<v.i>		BUILTIN BEGIN BREAK CONTINUE ELSE END FOR IF KSYM LEN SIZEOF STR STRNCMP USYM WHILE
 /* Functions and Map operators */
 %token  <v.i>		F_DELETE F_PRINT
 %token	<v.i>		MFUNC FUNC0 FUNC1 FUNCN OP1 OP2 OP4 MOP0 MOP1
@@ -256,6 +256,8 @@ func	: STR '(' factor ')'		{ $$ = ba_new($3, B_AT_FN_STR); }
 	| KSYM '(' expr ')'		{ $$ = ba_new($3, B_AT_FN_KSYM); }
 	| USYM '(' expr ')'		{ $$ = ba_new($3, B_AT_FN_USYM); }
 	| LEN '(' variable ')'		{ $$ = ba_new($3, B_AT_FN_LEN); }
+	| STRNCMP '(' expr ',' expr ',' expr ')'
+					{ $$ = ba_new(ba_append(ba_append($3, $5), $7), B_AT_FN_STRNCMP); }
 	;
 
 vargs	: expr
@@ -1060,6 +1062,7 @@ lookup(char *s)
 		{ "sizeof",	SIZEOF,		B_AT_FN_SIZEOF },
 		{ "stats",	MOP1,		B_AT_MF_STATS },
 		{ "str",	STR,		B_AT_FN_STR },
+		{ "strncmp",	STRNCMP,	B_AT_FN_STRNCMP },
 		{ "sum",	MOP1,		B_AT_MF_SUM },
 		{ "tid",	BUILTIN,	B_AT_BI_TID },
 		{ "time",	FUNC1,		B_AC_TIME },
--- usr.sbin/btrace/btrace.c
+++ usr.sbin/btrace/btrace.c
@@ -2398,6 +2398,7 @@ stmt_store(struct bt_stmt *bs, struct dt_evt *dtev)
 	case B_AT_BI_ARG0 ... B_AT_BI_ARG9:
 	case B_AT_FN_SIZEOF:
 	case B_AT_FN_LEN:
+	case B_AT_FN_STRNCMP:
 	case B_AT_FN_DEREF:
 	case B_AT_FN_SUBSCRIPT:
 	case B_AT_OP_PLUS ... B_AT_OP_SHR:
@@ -2592,6 +2593,7 @@ baeval(struct bt_arg *bval, struct dt_evt *dtev)
 	case B_AT_BI_RETVAL:
 	case B_AT_FN_SIZEOF:
 	case B_AT_FN_LEN:
+	case B_AT_FN_STRNCMP:
 	case B_AT_FN_DEREF:
 	case B_AT_FN_SUBSCRIPT:
 	case B_AT_OP_PLUS ... B_AT_OP_SHR:
@@ -2918,6 +2920,8 @@ ba_name(struct bt_arg *ba)
 		return "usym";
 	case B_AT_FN_LEN:
 		return "len";
+	case B_AT_FN_STRNCMP:
+		return "strncmp";
 	case B_AT_FN_DEREF: {
 		static char buf[64];
 		struct bt_deref *bd = ba->ba_value;
@@ -3082,6 +3086,16 @@ ba2long(struct bt_arg *ba, struct dt_evt *dtev)
 			val = (long)map_len((struct map *)bv->bv_value);
 		else
 			val = 0;
+		break;
+	}
+	case B_AT_FN_STRNCMP: {
+		char s1[STRLEN];
+		struct bt_arg *a1 = ba->ba_value;
+		struct bt_arg *a2 = SLIST_NEXT(a1, ba_next);
+		struct bt_arg *a3 = SLIST_NEXT(a2, ba_next);
+
+		strlcpy(s1, ba2str(a1, dtev), sizeof(s1));
+		val = strncmp(s1, ba2str(a2, dtev), (size_t)ba2long(a3, dtev));
 		break;
 	}
 	case B_AT_FN_SIZEOF: {
@@ -3305,6 +3319,7 @@ ba2str(struct bt_arg *ba, struct dt_evt *dtev)
 	}
 	case B_AT_FN_SIZEOF:
 	case B_AT_FN_LEN:
+	case B_AT_FN_STRNCMP:
 	case B_AT_OP_PLUS ... B_AT_OP_SHR:
 		snprintf(buf, sizeof(buf), "%ld", ba2long(ba, dtev));
 		str = buf;
@@ -3430,6 +3445,14 @@ ba2flags(struct bt_arg *ba)
 		break;
 	case B_AT_FN_LEN:
 		break;
+	case B_AT_FN_STRNCMP: {
+		struct bt_arg *a1 = ba->ba_value;
+		struct bt_arg *a2 = SLIST_NEXT(a1, ba_next);
+
+		flags |= ba2flags(a1);
+		flags |= ba2flags(a2);
+		break;
+	}
 	case B_AT_FN_DEREF:
 	case B_AT_FN_SUBSCRIPT:
 		/* We need the arg's register value (pointer) to dereference. */
